94936edaf9a0a0e4091c705da32d6d699a953a36
Facundo Juliá <facundodirie@gmail.com>
2023-05-06 08:14:35 -0300

    Nav-bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+
+        // primary colors
+
+        'green-primary': '#B9D74D',
+        'green-primary-dark': '#8BB741',
+        'green-secundary-dark': '#103A1E',
+        'green-bg':'EFF3E2',
+        'orange-strong':'#F39100',
+
+        // secundary 
+        'services-bg-color':'#F9C87F',
+        'nav-items-blue':'#50B1E3'
+      },
+      fontFamily:{
+        londrina:['Londrina Solid'],
+        poppins:['Poppins'],
+      }
+    },
   },
   plugins: [],
 }
