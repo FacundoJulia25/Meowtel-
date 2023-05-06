dff311f0feadd939a22c7d272037333f3f0a9523
Facundo Juliá <facundodirie@gmail.com>
2023-05-06 16:20:18 -0300

    beta

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,14 @@ module.exports = {
       colors:{
 
         // primary colors
-
         'green-primary': '#B9D74D',
         'green-primary-dark': '#8BB741',
         'green-secundary-dark': '#103A1E',
         'green-bg':'EFF3E2',
         'orange-strong':'#F39100',
-
+        'bg-primary': "#EFF3E2",
         // secundary 
-        'services-bg-color':'#F9C87F',
+        'services-bg-color':'#FEF2CB',
         'nav-items-blue':'#50B1E3'
       },
       fontFamily:{
